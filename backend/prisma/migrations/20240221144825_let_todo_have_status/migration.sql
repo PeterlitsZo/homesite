7114230e2cd94985df47a488e3e4fe3aff523c32
Peterlits Zo <peterlitszo@gmail.com>
2024-02-25 13:37:42 +0800

    feat (backend): Add table "todo_roots"
    * Add table "todo_roots", which means that the root node of all todo
      items and the place to keep the order of items.
    * To support the new table, we use the "postgres" rather than "prisma".

--- backend/prisma/migrations/20240221144825_let_todo_have_status/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "Status" AS ENUM ('DONE', 'TODO');
-
--- AlterTable
-ALTER TABLE "Todo" ADD COLUMN "status" "Status" NOT NULL DEFAULT 'TODO';
--- backend/src/infra/todo_repo.ts
+++ backend/src/infra/todo_repo.ts
@@ -1,9 +1,25 @@
-import { PrismaClient, Todo as PrismaTodo } from '@prisma/client';
+import postgres from 'postgres';
 
 import type { TodoRepo as DomainTodoRepo, TodoContent, TodoPatch } from "../domain/todo";
 import { Todo } from "../domain/todo";
 
-function todoToDomain(todo: PrismaTodo) {
+const databaseUrl = (function() {
+  const result = Bun.env.DATABASE_URL;
+  if (result === undefined) {
+    throw new Error('Environment variable DATABASE_URL not defined');
+  }
+  return result;
+})();
+
+const sql = postgres(databaseUrl);
+
+interface RawTodo {
+  id: number;
+  status: 'TODO' | 'DONE';
+  content: string;
+}
+
+function todoToDomain(todo: RawTodo) {
   return new Todo(
     JSON.stringify(todo.id),
     todo.status,
@@ -12,46 +28,66 @@ function todoToDomain(todo: PrismaTodo) {
 }
 
 export class TodoRepo implements DomainTodoRepo {
-  client: PrismaClient;
-
-  constructor() {
-    this.client = new PrismaClient();
-  }
+  constructor() {}
 
   async getTodo(id: string): Promise<Todo | null> {
-    const todo = await this.client.todo.findFirst({ where: { id: JSON.parse(id) } });
-    if (!todo) {
+    const todos = await sql`
+      SELECT * FROM todos
+        WHERE id = ${JSON.parse(id)};
+    `;
+    if (todos.length === 0) {
       return null;
     }
 
-    return todoToDomain(todo);
+    return todoToDomain(todos[0] as RawTodo);
   }
 
   async listTodos(): Promise<Todo[]> {
-    const todos = await this.client.todo.findMany();
+    const todos = await sql`
+      SELECT t.*
+        FROM todos t
+        JOIN todo_roots tr ON t.id = ANY(tr.todo_ids)
+        WHERE tr.name = 'default';
+    ` as RawTodo[];
+
     return todos.map(todo => {
       return todoToDomain(todo);
     });
   }
 
   async addTodo(content: TodoContent): Promise<Todo> {
-    const todo = await this.client.todo.create({
-      data: {
-        content: content.text,
-      }
-    })
-    return todoToDomain(todo);
+    const todos = await sql`
+      INSERT INTO todos (content)
+        VALUES (${content.text})
+        RETURNING *;
+    ` as RawTodo[];
+    await sql`
+      UPDATE todo_roots
+        SET todo_ids = ARRAY_APPEND(todo_ids, ${todos[0].id})
+        WHERE name = 'default';
+    `;
+
+    return todoToDomain(todos[0]);
   }
 
   async removeTodo(id: string) {
-    await this.client.todo.delete({ where: { id: JSON.parse(id) }});
+    await sql`
+      DELETE FROM todos WHERE id = ${JSON.parse(id)};
+    `;
+    await sql`
+      UPDATE todo_roots
+        SET todo_ids = ARRAY_REMOVE(todo_ids, ${JSON.parse(id)})
+        WHERE name = 'default';
+    `;
   }
 
   async updateTodo(id: string, patch: TodoPatch): Promise<Todo> {
-    let todo = await this.client.todo.update({
-      data: patch,
-      where: { id: JSON.parse(id) },
-    });
-    return todoToDomain(todo);
+    let todos = await sql`
+      UPDATE todos
+        SET status = ${patch.status}
+        WHERE id = ${JSON.parse(id)}
+        RETURNING *;
+    ` as RawTodo[];
+    return todoToDomain(todos[0]);
   }
 }
